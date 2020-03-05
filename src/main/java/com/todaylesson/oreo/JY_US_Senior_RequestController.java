7337eb70d9005ebdb633e34ab94835246e17f20b
xo951215 <x_o1215@naver.com>
2020-03-05 21:21:55 +0900

    z

--- src/main/java/com/todaylesson/oreo/JY_US_Senior_RequestController.java
+++ src/main/java/com/todaylesson/oreo/JY_US_Senior_RequestController.java
@@ -9,18 +9,18 @@ public class JY_US_Senior_RequestController {
 	
 	@RequestMapping("senior_request")
 	public String senior_Request_PopUp() {
-		return "jy_senior_request";
+		return "TodayLesson_UserPage/jy_senior_request";
 	}
 	
 
 	@RequestMapping("senior_request_form/{member_id}")
 	public String senior_Request_Button() {
-		return "jy_senior_form";
+		return "TodayLesson_UserPage/jy_senior_form";
 	}
 	
 	@RequestMapping("senior_switch/{member_id}")
 	public String senior_Form() {
-		return "jy_senior_switch";
+		return "TodayLesson_UserPage/jy_senior_switch";
 	}
 	
 }
