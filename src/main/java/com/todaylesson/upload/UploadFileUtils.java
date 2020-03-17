11855dc89c4176d0da7b6d6a670524062f07f774
Hahwasoo <gung4060@gmail.com>
2020-03-17 09:54:23 +0900

    ff

--- src/main/java/com/todaylesson/upload/UploadFileUtils.java
+++ src/main/java/com/todaylesson/upload/UploadFileUtils.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 import org.springframework.util.FileCopyUtils;
 import net.coobird.thumbnailator.Thumbnails;
 
-//�뤃�뜑 �깮�꽦?�� �뙆�씪 ���옣, �뜽�궡�씪 �깮�꽦�쓽 �옉�뾽�쓣 ��?�땲�떎.
+//�뤃�뜑 �깮�꽦?�� �뙆�씪 ���옣, �뜽�궡�씪 �깮�꽦�쓽 �옉�뾽�쓣 ��?�땲�떎.
 public class UploadFileUtils {
   
  static final int THUMB_WIDTH = 300;
