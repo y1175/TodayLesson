3fd3903aafb8d32223e015f0764fea2eeeaed6e9
y1175 <lordofbabo@gmail.com>
2020-03-23 01:09:06 +0900

    마이페이지 주문관리페이지 (만드는중)

--- src/main/java/com/todaylesson/oreo/User_YI_OrderList_Controller.java
+++ src/main/java/com/todaylesson/oreo/User_YI_OrderList_Controller.java
@@ -1,10 +1,16 @@
 package com.todaylesson.oreo;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.todaylesson.DTO.PageMaker;
+import com.todaylesson.DTO.SQLjoin_OrderList_Order_detail_MemberDTO;
 import com.todaylesson.service.User_YI_OrderList_Service;
 
 @Controller
@@ -13,9 +19,23 @@ public class User_YI_OrderList_Controller {
 	@Resource(name="user_YI_OrderList_Service")
 	private User_YI_OrderList_Service service;
 	
-	@RequestMapping("/todaylessonmypage/user_orderlist")
-	public String user_orderlist()
+	@RequestMapping("/todaylessonmypage/user_myorderlist")
+	public String user_orderlist(@RequestParam(required=false, defaultValue="") String start_date
+								,@RequestParam(required=false, defaultValue="") String end_date
+								,@RequestParam(required=false, defaultValue="1") int currPage
+								,Model model)
 	{
+		/*int totalCount= service.totalCount(start_date, end_date);
+		int pageSize=15;
+		int blockSize=5;
+		
+		PageMaker page=new PageMaker(currPage,totalCount,pageSize,blockSize);
+		
+		List<SQLjoin_OrderList_Order_detail_MemberDTO> list=service.orderlist(start_date,end_date);
+		model.addAttribute("list",list);*/
+		
+		
+		
 		return "/TodayLesson_UserPage/yi_us_orderlist";
 	}
 }
--- src/main/webapp/WEB-INF/views/TodayLesson_UserPage/yi_us_orderlist.jsp
+++ src/main/webapp/WEB-INF/views/TodayLesson_UserPage/yi_us_orderlist.jsp
@@ -9,13 +9,20 @@
 </head>
 <body>
 주문내역
-<form action="/todaylessonuser/user_orderlist_date">
+<form action="/todaylessonuser/user_myorderlist_date">
 <input type="date" name="start_date"> ~ <input type="date" name="end_date"> <input type="submit" value="조회">
 </form>
 
-<c:forEach var="item" items="${list}">
-주문일자:${item.orderlist_date }
-
+<c:forEach var="orderlist" items="${list}">
+주문일자:${orderlist.orderlist_date }	주문번호: ${orderlist.orderlist_no } 주문상태:${orderlist.orderlist_orderstatus}
+<input type="button" value="상세보기" onclick="location.href='/todaylessonuser/user_myorderdetail/${orderlist.orderlist_no}'">
+<c:forEach var="orderdetail" items="${detailList }">
+<img src="${orderdetail.product_thumb}" alt="thumb"><br>
+[스토어>${orderdetail.product_category}]<br>
+[상품명:${orderdetail.product_name}]<br>
+가격:${orderdetail.orderlist_cost}<br>
+<br>
+</c:forEach>
 </c:forEach>
 </body>
 </html>
