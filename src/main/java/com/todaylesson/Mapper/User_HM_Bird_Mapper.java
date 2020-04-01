aa8d99c48eda8098c321bcd51210bf6e42606354
kzoey <k_zoey94@naver.com>
2020-04-01 10:12:41 +0900

    햄이~

---
+++ src/main/java/com/todaylesson/Mapper/User_HM_Bird_Mapper.java
@@ -0,0 +1,12 @@
+package com.todaylesson.Mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface User_HM_Bird_Mapper {
+
+	
+	
+	
+}
---
+++ src/main/java/com/todaylesson/service/User_HM_Bird_Service.java
@@ -0,0 +1,5 @@
+package com.todaylesson.service;
+
+public interface User_HM_Bird_Service {
+
+}
