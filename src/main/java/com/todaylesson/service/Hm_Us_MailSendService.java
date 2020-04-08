c3a843357efb0d8c3d99a7671741dac62d088b56
kzoey <k_zoey94@naver.com>
2020-04-08 16:48:44 +0900

    이메일

--- src/main/java/com/todaylesson/service/Hm_Us_MailSendService.java
+++ src/main/java/com/todaylesson/service/Hm_Us_MailSendService.java
@@ -67,11 +67,12 @@ public class Hm_Us_MailSendService {
 			String key = getKey(false, 6);
 					
 			MimeMessage mail = mailSender.createMimeMessage();
-			String htmlStr = "<h2>안녕하세요 '"+ member_id +"' 님</h2><br><br>" 
-					+ "<p>비밀번호 찾기를 신청해주셔서 임시 비밀번호를 발급해드렸습니다.</p>"
-					+ "<p>임시로 발급 드린 비밀번호는 <h2 style='color : blue'>'" + key +"'</h2>이며 로그인 후 마이페이지에서 비밀번호를 변경해주시면 됩니다.</p><br>"
-					+ "<h3><a href='http://54.180.123.73/MS/'>TodayLesson :p 홈페이지 접속 ^0^</a></h3><br><br>"
-					+ "(혹시 잘못 전달된 메일이라면 이 이메일을 무시하셔도 됩니다)";
+			String htmlStr = "<h2 style='text-align : center'>안녕하세요 '"+ member_id +"' 님</h2><br><br>" 
+					+ "<p style='text-align : center'>비밀번호 찾기를 신청해주셔서 임시 비밀번호를 발급해드렸습니다.</p>"
+					+ "<p style='text-align : center'>임시로 발급 드린 비밀번호는 <h2 style='text-align : center'>'" + key +"'</h2></p>" 
+					+ "<p style='text-align : center'>이며 로그인 후 마이페이지에서 비밀번호를 변경해주시면 됩니다.</p><br>"
+					+ "<h3 style='text-align : center'><a href='http://localhost:8080/todaylessonlogin'>TodayLesson :p 홈페이지 접속 ^0^</a></h3><br><br>"
+					+ "<p style='text-align : center'>혹시 잘못 전달된 메일이라면 이 이메일을 무시하셔도 됩니다</p>";
 			try {
 				mail.setSubject("[TodayLesson] 임시 비밀번호가 발급되었습니다", "utf-8");
 				mail.setText(htmlStr, "utf-8", "html");
@@ -80,6 +81,7 @@ public class Hm_Us_MailSendService {
 			} catch (MessagingException e) { 
 				e.printStackTrace();
 			}
+			
 			key=encoder.encode(key);
 			// 비밀번호 암호화
 			/*key = UserSha256.encrypt(key);*/
